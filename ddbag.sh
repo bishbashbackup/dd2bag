40cfd2e5a1c7c248c504b9129eb12e31fedf6ff4
Alex Habgood <57497331+bishbashbackup@users.noreply.github.com>
2024-04-24 13:59:50 +0100

    Update ddbag.sh

--- ddbag.sh
+++ ddbag.sh
@@ -2,91 +2,186 @@
 
 # Script created 18/04/2024 7:26
 
+VERSION="0.2"
+SCRIPTNAME="ddbag"
+SCRIPTDIR="$(dirname ${0})"
+DATE="$(date +%m-%d-%Y-%T)"
+count=1
 
-# Sets up aip variable based on stdin
-aip="$1"
 
-# Checks if /mnt/dd2bag exists and is empty
-if [ -d "/mnt/dd2bag" ]; then
-    if [ "$(ls -A /mnt/dd2bag)" ]; then
-        echo "Error: The mountpoint /mnt/dd2bag is not empty. Exiting."
-        exit 1
-    else
-        echo "/mnt/dd2bag exists but is empty. Proceeding."
-    fi
+_usage() { 
+	echo "script usage: ${SCRIPTDIR}/${SCRIPTNAME}_${VERSION}.sh [-h]" 
+}
+
+_help(){
+	cat <<EOF
+${SCRIPTNAME}
+
+Usage: ddbag [options] <AIP> <target> 
+
+<AIP> = The identifier you are giving to the bag that will be created containing disk images. This string can only contain alphanumeric characters, periods, hyphens and underscores.
+
+<target> = This is a folderpath where the bag will be created. This is optional. If a folderpath isn't given then it will default to the present working directory.
+
+Examples: ddbag 12345 '/home/bishbashbackup/Documents'
+          ddbag -m package23 '/home/bishbashbackup/Documents'
+
+Creates a disk image and then packages in a bagit format
+
+The default disk drive is set to: "/dev/sr0". If yours is different, this can be changed in the config.txt file.
+
+Options:
+ -m, --multidisk		enables prompt to image more than one disk
+ 
+
+EOF
+}
+
+# Transforms long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    '--help')			set -- "$@" '-h'   ;;
+    '--multidisk')		set -- "$@" '-m'   ;;
+    *)					set -- "$@" "$arg" ;;
+  esac
+done
+
+# Default behavior
+multidisk=false 
+
+# Parse short options
+OPTIND=1
+while getopts "hm" opt ; do
+	case "$opt" in
+		'h') _help ; exit 0 ;;
+		'm') multidisk=true ;;
+		'?') _usage >&2; exit 1 ;;
+	esac
+done
+shift "$((OPTIND-1))"
+
+
+
+# Load in variable(s) from config.txt
+source "$SCRIPTDIR/config.txt"
+
+# Load in variable from first positional parameter
+if [[ -n "$1" && "$1" =~ ^[[:alnum:]._-]+$ ]]; then
+	AIP="$1"
 else
-    read -p "/mnt/dd2bag does not exist. Would you like to create and use it? (y/n): " choice
-    if [[ $choice =~ ^[Yy]$ ]]; then
-        mkdir -p /mnt/dd2bag
-    else
-        echo "Exiting."
-        exit 1
-    fi
+	echo "Invalid Input - AIP identifier can only contain alphanumeric characters, periods, underscores and hyphens"
+	exit 1
+fi
+
+# Load in variable from second positional parameter (optional)
+if [ -n "$2" ]; then
+	if [ -d "$2" ]; then
+		target="$2"
+	else
+		echo "Invalid target folder"
+		exit 1
+	fi
+else
+	target="$(pwd)"
+fi
+
+# Initial checks to see if folder already exists with AIP name in target directory. 
+if [ -d "$target/$AIP" ]; then
+	echo "Folder named $AIP already exists in target directory."
+	exit 1
 fi
 
 # Function to check and handle errors
-check_error() {
+_check_error() {
     if [ $? -ne 0 ]; then
         echo "Error occurred during $1. Exiting."
         exit 1
     fi
 }
 
+# Function to create disk images
+_disk-image() {
+	# Check if the disk is present using blkid
+	while true; do
+		if blkid /dev/sr0 &>/dev/null; then
+			ddrescue -d -r3 $DRIVE $tempdir/"$AIP"/"$AIP"_"$count".dd $tempdir/"$AIP"/"$AIP"_"$count"_ddrescue.log
+#			echo "Created disk image $count"
+			break
+		else
+			read -p "Error: Disk not found. Try again? (Answer yes or no): " choice
+			if [[ "$choice" =~ ^(y|Y|yes|Yes|YES)$ ]]; then
+				echo "Trying again..."
+				sleep 3			
+			elif [[ "$choice" =~ ^(n|N|no|No|NO)$ ]]; then
+				if [ -z "$(ls -A $tempdir/$AIP)" ]; then
+					echo "Exiting script"
+					exit 0
+				else
+					break
+				fi
+			else
+				echo "Invalid input. Please enter 'yes' or 'no'."
+			fi
+		fi
+	done
+}
+
+# Function to check for multidisk imaging
+_multi-image() {
+	while true; do
+		eject
+		echo "Do you want to create another disk image? (Answer yes or no - if yes insert new disk before answering): "
+			read choice
+
+		if [[ "$choice" =~ ^(y|Y|yes|Yes|YES)$ ]]; then
+			((count++))
+			_disk-image
+			continue
+			
+		elif [[ "$choice" =~ ^(n|N|no|No|NO)$ ]]; then
+			break
+		
+		else
+			echo "Invalid input. Please enter 'yes' or 'no'."
+		fi
+	done		
+}
+
+
 # Function to cleanup after script execution
-cleanup() {
-    if [ -d $mountpoint ]; then
-        sudo umount $mountpoint &> /dev/null
-        rmdir $mountpoint
-		rm -r $tempdir
+_cleanup() {
+    if [ -d $tempdir ]; then
+        rm -r $tempdir
     fi
 }
 
 # Trap for cleanup on script exit
-trap cleanup EXIT
+trap _cleanup EXIT
+
 
 # Create temporary directory for creating the bag
-mountpoint="/mnt/dd2bag"
-tempdir="$(mktemp -d $(pwd)/temp-XXXXX)"
-check_error "making the temp directory"
+tempdir=$(mktemp -d "$target/ddbag_XXXX")
+_check_error "making the temp directory"
 
-# Mount CD drive as read-only
-sudo mount -o ro /dev/sr0 $mountpoint
-check_error "mounting CD drive"
+#Make subdirectory for disk image
+mkdir $tempdir/"$AIP"
 
-# Create disk image using ddrescue
-sudo ddrescue $mountpoint $tempdir/"$aip".dd
-check_error "creating disk image"
+# Create disk image using ddrescue, with loop for imaging multiple disks
+_disk-image
+_check_error "disk imaging"
+if [[ "$multidisk" == true ]]; then
+	_multi-image
+	_check_error "multidisk imaging"
+fi
 
-# Eject the disk
-eject /dev/sr0
-check_error "ejecting disk"
-
+echo "Disk Imaging complete! You can remove the disk. Tranferring to bag..."
+		
 # Create BagIt folder structure
-python3 -m bagit --contact-name $tempdir/"$aip"
+python3 -m bagit --quiet $tempdir/"$AIP"
+_check_error "Creating Bagit Structure"
 
-# Move disk image into BagIt folder structure
-mv /path/to/disk_image.iso /path/to/bagit/data/disk_image.iso
-check_error "moving disk image to BagIt folder"
+# Move Bagity out of temp directory
+mv $tempdir/"$AIP" $target
 
-# Mount disk image as read-only
-sudo mount -o ro,loop /path/to/bagit/data/disk_image.iso /mnt/disk_image
-check_error "mounting disk image"
-
-# Check disk image for viruses using clamscan
-clamscan -r /mnt/disk_image
-check_error "scanning disk image with clamscan"
-
-# Use DROID to create a list of all files on the disk
-droid -R /mnt/disk_image > /path/to/bagit/metadata/file_list.txt
-check_error "creating file list with DROID"
-
-# Move final reports to top level of BagIt structure
-mv /path/to/bagit/metadata/file_list.txt /path/to/bagit/
-mv /var/log/clamav/clamscan.log /path/to/bagit/
-
-# Unmount disk image and CD drive
-sudo umount /mnt/disk_image
-sudo umount /mnt/dd2bag
-check_error "unmounting disk image and CD drive"
-
-echo "Process completed successfully."
+echo "Bagging complete!"
